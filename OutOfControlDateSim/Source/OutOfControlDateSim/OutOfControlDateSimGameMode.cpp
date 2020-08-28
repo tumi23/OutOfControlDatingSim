c43121620cde6b60ff9c6667ba10246b155a2fa8
Tumi <tumi@myrkur.is>
2020-08-28 21:46:48 +0000

    Update OutOfControlDateSimGameMode.cpp

--- OutOfControlDateSim/Source/OutOfControlDateSim/OutOfControlDateSimGameMode.cpp
+++ OutOfControlDateSim/Source/OutOfControlDateSim/OutOfControlDateSimGameMode.cpp
@@ -8,10 +8,4 @@
 AOutOfControlDateSimGameMode::AOutOfControlDateSimGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	// use our custom HUD class
-	HUDClass = AOutOfControlDateSimHUD::StaticClass();
 }
